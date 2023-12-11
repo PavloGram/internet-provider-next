7ecfc20a769f2e2a1d1f81eb87a36c0b8697b2a6
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-12-11 21:45:43 +0200

    debug size pay block

--- app/pay/page.jsx
+++ app/pay/page.jsx
@@ -11,7 +11,7 @@ function Pay() {
       <Hero />
       <section>
         <div className="max-w-1140px flex justify-center items-center p-[90px]">
-          <div className="relative h-[300px] min-w-[450px] max-w-[600px] flex justify-center items-center shadow-[0_0_12px_0_rgba(0,0,0,0.2)] rounded-xl overflow-hidden">
+          <div className="relative h-[300px] min-w-[350px] w-[450px]  flex justify-center items-center shadow-[0_0_12px_0_rgba(0,0,0,0.2)] rounded-xl overflow-hidden">
             <Link
               href="/"
               className={`${styles.linkWrap} absolute  w-full h-full transition-colors duration-300 hover:bg-black/70 flex flex-col justify-center items-center gap-y-5`}
